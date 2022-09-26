ed11704001b19dc22fe16debb09b002f431f78dc
Antony Della Vecchia <vecchia@blofield.math.tu-berlin.de>
2022-09-26 16:29:56 +0200

    Updates

---
+++ precache-manifest.91fcbdd25d88aaf7af1c189fed75384f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a36154602a46e783de00179ac4b9f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd72868076a54633389f",
+    "url": "/static/css/2.9d67ac4b.chunk.css"
+  },
+  {
+    "revision": "132816c433bd3ad717fe",
+    "url": "/static/css/main.d612f22b.chunk.css"
+  },
+  {
+    "revision": "bd72868076a54633389f",
+    "url": "/static/js/2.b731ed15.chunk.js"
+  },
+  {
+    "revision": "132816c433bd3ad717fe",
+    "url": "/static/js/main.6e8d519b.chunk.js"
+  },
+  {
+    "revision": "546d3803ef13d19ff98d",
+    "url": "/static/js/runtime~main.2e7a2919.js"
+  }
+]);
