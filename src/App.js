0248fac6d1fbb19e34fc9a4b92bf10bf7984deff
Antony <antonydellavecchia@gmail.com>
2022-09-27 18:13:08 +0200

    some updates

--- src/App.js
+++ src/App.js
@@ -8,25 +8,35 @@ import RayMarching from './shaders/ray-marching.glsl'
 function App() {
   return (
     <div>
-      <div className="content">
-      <section className="hero">
-        <div className="hero-body">
-          <p className="title">
-            Antony Della Vecchia
-          </p>
-          <p className="subtitle">
-            <a href="https://github.com/antonydellavecchia">github</a>
-          </p>
-        </div>
-        
-      </section>
+      <div className="content ">
+        <section className="hero">
+          <div className="hero-body">
+            <p className="title has-text-light">
+              Antony Della Vecchia
+            </p>
+            <p className="subtitle has-text-light">
+              <a href="https://github.com/antonydellavecchia">github</a>
+            </p>
+            <p className="subtitle has-text-light">
+              Office: Strasse des 17. Juni 136, Room 626
+            </p>
+            <p className="subtitle has-text-light">
+              Email: vecchia@math.tu-berlin.de
+            </p>
 
-      <section className="section ">
-        <h1 className="title">Section</h1>
-        <h2 className="subtitle">
-          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
-        </h2>
-         </section>
+          </div>
+          
+        </section>
+
+        <section className="section">
+          <h2 className="title has-text-light">About Me</h2>
+          <p className="subtitle has-text-light">
+            I am a member of <a href="https://www.math.tu-berlin.de/fachgebiete_ag_diskalg/fg_diskrete_mathematik_geometrie/v-menue/diskrete_mathematik_geometrie/">
+                               The Discrete Geometry Group </a>
+            of <a href="https://page.math.tu-berlin.de/~joswig/"> Michael Joswig </a> at the <a href="https://www.math.tu-berlin.de/menue/home/">  Institute of Mathematics of the Technical University in Berlin</a>. <br></br>
+            I am currently an employee of <a href="https://www.mardi4nfdi.de/about/mission">MaRDI</a> working on serialization and databases for computer algebra.
+          </p>
+        </section>
       </div>
       <ThreeContainer backgroundShader={RayMarching}/>
     </div>
--- src/shaders/ray-marching.glsl
+++ src/shaders/ray-marching.glsl
@@ -7,90 +7,6 @@ uniform vec2 u_mouse;
 uniform float u_time;
 varying vec4 vUv;
 
-float weighted_dot(vec3 p, vec3 q) {
-  float ratio = u_resolution.x / u_resolution.y;
-  return pow(ratio, 2.0) * p.x * q.x + p.y * q.y + p.z * q.z;
-}
-
-float distance_from_sphere(in vec3 p, in vec3 c, float r)
-{
-  float f_0 = p.y - p.x * p.x;
-  float f_1 = p.z - pow(p.y, 3.0);
-  //return max(f_0, f_1);
-  return sqrt(weighted_dot(p - c, p - c)) - r;
-}
-
-float map_the_world(in vec3 p)
-{
-    float sphere_0 = distance_from_sphere(p, vec3(0.0), 1.0);
-
-    // Later we might have sphere_1, sphere_2, cube_3, etc...
-
-    return sphere_0;
-}
-
-vec3 calculate_normal(in vec3 p)
-{
-    const vec3 small_step = vec3(0.001, 0.0, 0.0);
-
-    float gradient_x = map_the_world(p + small_step.xyy) - map_the_world(p - small_step.xyy);
-    float gradient_y = map_the_world(p + small_step.yxy) - map_the_world(p - small_step.yxy);
-    float gradient_z = map_the_world(p + small_step.yyx) - map_the_world(p - small_step.yyx);
-
-    vec3 normal = vec3(gradient_x, gradient_y, gradient_z);
-
-    return normalize(normal);
-}
-
-
-
-
-
-
-vec3 ray_march(in vec3 ro, in vec3 rd)
-{
-    float total_distance_traveled = 0.0;
-    const int NUMBER_OF_STEPS = 32;
-    const float MINIMUM_HIT_DISTANCE = 1.0;
-    const float MAXIMUM_TRACE_DISTANCE = 1000.0;
-    vec3 color = vec3(0.0, 0.0, 1.0);
-    
-    for (int i = 0; i < NUMBER_OF_STEPS; ++i)
-    {
-        // Calculate our current position along the ray
-        vec3 current_position = ro + total_distance_traveled * rd;
-
-        // We wrote this function earlier in the tutorial -
-        // assume that the sphere is centered at the origin
-        // and has unit radius
-        float distance_to_closest = distance_from_sphere(current_position, vec3(0.0, 0.0, 0.0), 1.0);
-
-        if (distance_to_closest < MINIMUM_HIT_DISTANCE) // hit
-        {
-
-	  vec3 normal = calculate_normal(current_position);
-	  vec3 light_position = vec3(2.0, -5.0 * cos(u_time), 3.0);
-	  vec3 direction_to_light = normalize(current_position - light_position);
-
-	  float diffuse_intensity = max(0.0, dot(normal, direction_to_light));
-
-	  return vec3(1.0, 0.0, 0.0) * diffuse_intensity;
-        }
-
-        if (total_distance_traveled > MAXIMUM_TRACE_DISTANCE) // miss
-        {
-	  break;
-        }
-
-        // accumulate the distance traveled thus far
-        total_distance_traveled += distance_to_closest;
-    }
-
-    // If we get here, we didn't hit anything so just
-    // return a background color (black)
-    return color;
-}
-
 vec4 quat_from_axis_angle(vec3 axis, float angle)
 { 
   vec4 qr;
@@ -130,13 +46,13 @@ vec3 rotate_vertex_position(vec3 position, vec3 axis, float angle)
 }
 
 float polynomial(vec3 normal) {
-  return pow(normal.x, 3.0) - pow(normal.y, 2.0) * normal.x + 3.0 * cos(sqrt(2.0) * u_time) *normal.z * normal.x * normal.y * 10.0 * u_time * sin(1.0);
+  return pow(normal.x, 2.0) * normal.z - pow(normal.y, 2.0) * normal.x + 3.0 * cos(sqrt(2.0) * u_time) *normal.z * normal.x * normal.y * 10.0 * u_time * sin(1.0);
 }
 
 void main() {
   vec3 ray_origin = vec3(0.0, 0.0, 1.0);
   vec2 uv =  gl_FragCoord.xy / u_resolution.xy;
   vec3 P = rotate_vertex_position(normalize(vUv.xyz), vec3(0.0, cos(u_time),sin(u_time)), 10.0 * u_time);
-  vec3 color = vec3(step(abs(polynomial(P)), 0.05), step(abs(polynomial(P)), 0.05), P.z);
-  gl_FragColor = vec4(abs(color), 1.0);
+  vec3 color = vec3(0.5, 0.3 * step(abs(polynomial(P)), 0.05) + 0.7, P.z * P.y * 0.5);
+  gl_FragColor = vec4(abs(color), 0.3);
 }
